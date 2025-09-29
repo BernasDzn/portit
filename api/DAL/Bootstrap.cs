cb474256f35f44227fff7446d81a3e3747e83712
BernasDzn <bernardogranjacardoso@gmail.com>
2025-09-29 21:38:39 +0100

    feat(Vessel) add search by name, IMO and owner
    refs: #2

--- api/DAL/Bootstrap.cs
+++ api/DAL/Bootstrap.cs
@@ -111,13 +111,13 @@ public static class Bootstrap
 
         // Add Bootstrap data
         context.Vessels.AddRange(
-            new Vessel(Guid.NewGuid(), "Ever Given", "IMO1234567",
+            new Vessel(Guid.NewGuid(), "Ever Given", "IMO 7585229",
             new VesselType(Guid.NewGuid(), "Panamax", "Max size for Panama Canal", 20, 10, 5),
             252482890),
-            new Vessel(Guid.NewGuid(), "Maersk Triple E", "IMO7654321",
+            new Vessel(Guid.NewGuid(), "Maersk Triple E", "IMO 3815389",
             new VesselType(Guid.NewGuid(), "Post-Panamax", "Larger than Panamax", 30, 15, 7),
             252482890),
-            new Vessel(Guid.NewGuid(), "CMA CGM Marco Polo", "IMO1122334",
+            new Vessel(Guid.NewGuid(), "CMA CGM Marco Polo", "IMO 6699530",
             new VesselType(Guid.NewGuid(), "Ultra Large Container Vessel (ULCV)", "Largest container ships", 40, 20, 10),
             252482918)
         );
