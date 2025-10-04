5e5eca2234cb7ae90f1e3e75bdf24043e73e39bd
Rui Santiago <1221402@isep.ipp.pt>
2025-10-04 16:19:36 +0100

    chore(Domain): add staff domain entities and DTO equivalents
    Refs: #16

---
+++ api/Application/DTO/OperationalWindowDto.cs
@@ -0,0 +1,7 @@
+public class OperationalWindowDto
+{
+	public DayOfWeek StartWeekDay { get; set; }
+	public DayOfWeek EndWeekDay { get; set; }
+	public TimeOnly DayStartTime { get; set; }
+	public TimeOnly DayEndTime { get; set; }
+}
