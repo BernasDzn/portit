c24494de6d9c744ad93016a0c881d6aec377c823
BernasDzn <bernardogranjacardoso@gmail.com>
2025-10-05 16:16:31 +0100

    fix(Designation): allow parenthesis and dot
    refs:

--- BackEnd/Api/Domain/ValueObjects/Designation.cs
+++ BackEnd/Api/Domain/ValueObjects/Designation.cs
@@ -20,7 +20,7 @@ public class Designation
 				throw new ArgumentException("Designation cannot be null or empty", nameof(value));
 			if (trimmed.Length > 100 || trimmed.Length < 2)
 				throw new ArgumentException("Designation must be between 2 and 100 characters", nameof(value));
-			if (!Regex.IsMatch(trimmed, @"^[\p{L}0-9 \-]+$", RegexOptions.None))
+			if (!Regex.IsMatch(trimmed, @"^[\p{L}0-9 .\-()]+$", RegexOptions.None))
 				throw new ArgumentException("Designation can only contain alphanumeric characters, spaces, and hyphens", nameof(value));
 
 			_value = trimmed;
