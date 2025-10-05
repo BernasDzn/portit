df10f5d92091f2b5df3f1c7b068acc87b467d20b
F r a n c i s c o <1231092@isep.ipp.pt>
2025-10-05 17:03:34 +0100

    chore: added update tests to dock
    Refs: #11

--- BackEnd/Tests/Domain/DockTest.cs
+++ BackEnd/Tests/Domain/DockTest.cs
@@ -69,13 +69,68 @@ public class DockTest
         );
 
         if (!useVesselTypes)
-        {
             Assert.Throws<ArgumentException>(() => new Dock(Guid.NewGuid(), new Designation { Value = "Valid Name" }, new Designation { Value = "Valid Location" },
                 new PhysicalCharacteristics { Length = 500, Depth = 20, Draft = 20 },
                 new HashSet<VesselType> { }
             ));
-            Assert.Throws<ArgumentException>(() => validDock.UpdateVesselTypes(vesselTypesParam));
-            Assert.Throws<ArgumentException>(() => validDock.UpdateVesselTypes(new HashSet<VesselType> { }));
-        }
+    }
+
+
+    //---UPDATE DOCK TEST---
+    [Fact]
+    public void WhenUpdatingDockWithValidParameters_ThenIsUpdatedSuccessfully()
+    {
+        Dock dockToUpdate = validDock;
+
+        dockToUpdate.UpdateName("New Valid Name");
+        dockToUpdate.UpdateLocation("New Valid Location");
+        dockToUpdate.UpdatePhysicalCharacteristics(new PhysicalCharacteristics { Length = 600, Depth = 25, Draft = 22 });
+
+        VesselType vt3 = new VesselType(Guid.NewGuid(), new Designation { Value = "New Type" }, new Designation { Value = "New Description" }, 25, 12, 6, new PhysicalCharacteristics { Length = 350, Depth = 16, Draft = 13 });
+        HashSet<VesselType> newVesselTypes = new HashSet<VesselType> { vt1, vt3 };
+        dockToUpdate.UpdateVesselTypes(newVesselTypes);
+
+        Assert.Equal("New Valid Name", dockToUpdate.Name.Value);
+        Assert.Equal("New Valid Location", dockToUpdate.Location.Value);
+        Assert.Equal(600, dockToUpdate.PhysicalCharacteristics.Length);
+        Assert.Equal(25, dockToUpdate.PhysicalCharacteristics.Depth);
+        Assert.Equal(22, dockToUpdate.PhysicalCharacteristics.Draft);
+        Assert.Contains(vt1, dockToUpdate.SupportedVesselTypes);
+        Assert.Contains(vt3, dockToUpdate.SupportedVesselTypes);
+        Assert.DoesNotContain(vt2, dockToUpdate.SupportedVesselTypes);
+    }
+
+    //---UPDATE DOCK TEST WITH INVALID PARAMETERS---
+    [Theory]
+    [InlineData("", "New Valid Location", 600, 25, 22)]
+    [InlineData("A Dock Name That Is Way Too Long To Be Considered Valid Because It Exceeds The Maximum Length Allowed", "New Valid Location", 600, 25, 22)]
+    [InlineData("New Valid Name", "", 600, 25, 22)]
+    [InlineData("New Valid Name", "A Location Name That Is Way Too Long To Be Considered Valid Because It Exceeds The Maximum Length Allowed", 600, 25, 22)]
+    [InlineData("New Valid Name", "New Valid Location", 0, 25, 22)]
+    [InlineData("New Valid Name", "New Valid Location", -1, 25, 22)]
+    [InlineData("New Valid Name", "New Valid Location", 600, 0, 22)]
+    [InlineData("New Valid Name", "New Valid Location", 600, -1, 22)]
+    [InlineData("New Valid Name", "New Valid Location", 600, 25, 0)]
+    [InlineData("New Valid Name", "New Valid Location", 600, 25, -1)]
+    public void WhenUpdatingDockWithInvalidParameters_ThenThrowsException(string dockName, string dockLocation, double length, double depth, double draft)
+    {
+        Dock dockToUpdate = validDock;
+
+        // Invalid name
+        Assert.Throws<ArgumentException>(() =>
+        {
+            dockToUpdate.UpdateName(dockName);
+            dockToUpdate.UpdateLocation(dockLocation);
+            dockToUpdate.UpdatePhysicalCharacteristics(new PhysicalCharacteristics { Length = length, Depth = depth, Draft = draft });
+        });
+    }
+
+    [Fact]
+    public void WhenUpdatingDockWithInvalidVesselTypes_ThenThrowsException()
+    {
+        Dock dockToUpdate = validDock;
+
+        Assert.Throws<ArgumentException>(() => dockToUpdate.UpdateVesselTypes(null!));
+        Assert.Throws<ArgumentException>(() => dockToUpdate.UpdateVesselTypes(new HashSet<VesselType> { }));
     }
 }
