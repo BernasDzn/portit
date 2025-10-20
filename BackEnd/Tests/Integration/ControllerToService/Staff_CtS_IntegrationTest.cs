2dc2ed8ac1ee803c76cb60bcd7d29ef82d557aff
Rui Santiago <1221402@isep.ipp.pt>
2025-10-20 23:00:04 +0100

    chore(test): Finish staff integration tests for controller to service scope

--- BackEnd/Tests/Integration/ControllerToService/Staff_CtS_IntegrationTest.cs
+++ BackEnd/Tests/Integration/ControllerToService/Staff_CtS_IntegrationTest.cs
@@ -7,6 +7,8 @@ using Api.Domain.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Api.Application.DataTransfer.Filters;
+using Api.Infrastructure.Utilities;
 
 namespace Tests.Integration.ControllerToService;
 
@@ -69,6 +71,27 @@ public class Staff_CtS_IntegrationTest
 	}
 
 	[Fact]
+	public async Task GetAll_ReturnsOkWithListOfStaffs()
+	{
+		_staffRepoMock.Setup(r => r.GetStaffsAsync())
+			.ReturnsAsync(new List<Staff> { _staff_mock });
+
+		var result = await _controller.GetAll();
+		var response = Assert.IsType<OkObjectResult>(result.Result);
+		var value = Assert.IsType<List<StaffDto>>(response.Value);
+	}
+
+	[Fact]
+	public async Task GetAll_ShouldReturnBadRequest_OnException()
+	{
+		_staffRepoMock.Setup(r => r.GetStaffsAsync())
+			.ThrowsAsync(new Exception("Test Exception"));
+
+		var result = await _controller.GetAll();
+		Assert.IsType<BadRequestObjectResult>(result.Result);
+	}
+
+	[Fact]
 	public async Task Create_ReturnsCreatedStaff()
 	{
 		_qualRepoMock.Setup(r => r.GetQualificationByIdAsync(It.IsAny<string>()))
@@ -96,4 +119,133 @@ public class Staff_CtS_IntegrationTest
 		Assert.IsType<BadRequestObjectResult>(result.Result);
 	}
 
+	[Fact]
+	public async Task Create_ShouldReturnBadRequest_OnException()
+	{
+		_qualRepoMock.Setup(r => r.GetQualificationByIdAsync(It.IsAny<string>()))
+			.ReturnsAsync(_qualification_mock);
+
+		_staffRepoMock.Setup(r => r.Add(It.IsAny<Staff>()))
+			.ThrowsAsync(new Exception("Test Exception"));
+
+		var result = await _controller.Create(_createStaffDto_mock);
+		Assert.IsType<BadRequestObjectResult>(result.Result);
+	}
+
+	[Fact]
+	public async Task Create_ShouldReturnBadRequest_WhenQualificationNotFound()
+	{
+		_staffRepoMock.Setup(r => r.GetStaffByMecNumberAsync(It.IsAny<string>()))
+			.ReturnsAsync(_staff_mock);
+
+		_qualRepoMock.Setup(r => r.GetQualificationByIdAsync(It.IsAny<string>()))
+			.ReturnsAsync((Qualification)null!);
+
+		var result = await _controller.Create(_createStaffDto_mock);
+		Assert.IsType<BadRequestObjectResult>(result.Result);
+	}
+
+	[Fact]
+	public async Task Deactivate_ReturnsOk_WithValidData()
+	{
+		_staffRepoMock.Setup(r => r.GetStaffByMecNumberAsync(It.IsAny<string>()))
+			.ReturnsAsync(_staff_mock);
+
+		_staffRepoMock.Setup(r => r.Update(It.IsAny<Staff>()))
+			.ReturnsAsync(_staff_mock);
+
+		var result = await _controller.Deactivate(_staff_mock.MechanograficNumber.Value);
+		var response = Assert.IsType<OkObjectResult>(result);
+		var value = Assert.IsType<StaffDto>(response.Value);
+	}
+
+	[Fact]
+	public async Task Deactivate_ShouldReturnBadRequest_OnException()
+	{
+		_staffRepoMock.Setup(r => r.GetStaffByMecNumberAsync(It.IsAny<string>()))
+			.ThrowsAsync(new Exception("Test Exception"));
+
+		var result = await _controller.Deactivate(_staff_mock.MechanograficNumber.Value);
+		Assert.IsType<BadRequestObjectResult>(result);
+	}
+
+	[Fact]
+	public async Task Deactivate_ShouldReturnBadRequest_WhenStaffNotFound()
+	{
+		_staffRepoMock.Setup(r => r.GetStaffByMecNumberAsync(It.IsAny<string>()))
+			.ReturnsAsync((Staff)null!);
+
+		var result = await _controller.Deactivate(_staff_mock.MechanograficNumber.Value);
+		Assert.IsType<BadRequestObjectResult>(result);
+	}
+
+	[Fact]
+	public async Task Update_ReturnsOk_WithValidData()
+	{
+		_qualRepoMock.Setup(r => r.GetQualificationByIdAsync(It.IsAny<string>()))
+			.ReturnsAsync(_qualification_mock);
+
+		_staffRepoMock.Setup(r => r.GetStaffByMecNumberAsync(It.IsAny<string>()))
+			.ReturnsAsync(_staff_mock);
+
+		_staffRepoMock.Setup(r => r.Update(It.IsAny<Staff>()))
+			.ReturnsAsync(_staff_mock);
+
+		var result = await _controller.Update(_staff_mock.MechanograficNumber.Value, _createStaffDto_mock);
+		var response = Assert.IsType<OkObjectResult>(result.Result);
+		var value = Assert.IsType<StaffDto>(response.Value);
+	}
+
+	[Fact]
+	public async Task Update_ShouldReturnBadRequest_OnException()
+	{
+		_staffRepoMock.Setup(r => r.GetStaffByMecNumberAsync(It.IsAny<string>()))
+			.ThrowsAsync(new Exception("Test Exception"));
+
+		var result = await _controller.Update(_staff_mock.MechanograficNumber.Value, _createStaffDto_mock);
+		Assert.IsType<BadRequestObjectResult>(result.Result);
+	}
+
+	[Fact]
+	public async Task Update_ShouldReturnBadRequest_WhenStaffNotFound()
+	{
+		_staffRepoMock.Setup(r => r.GetStaffByMecNumberAsync(It.IsAny<string>()))
+			.ReturnsAsync((Staff)null!);
+
+		var result = await _controller.Update(_staff_mock.MechanograficNumber.Value, _createStaffDto_mock);
+		Assert.IsType<BadRequestObjectResult>(result.Result);
+	}
+
+	[Fact]
+	public async Task Filter_ShouldReturnOk_WhenFound()
+	{
+		var page = Page<Staff>.Of(new List<Staff> { _staff_mock }, new Pageable { PageNumber = 1, PageSize = 10 });
+		_staffRepoMock.Setup(r => r.FilterStaffsAsync(It.IsAny<StaffFilter>()))
+			.ReturnsAsync(page);
+
+		var result = await _controller.Filter(new StaffFilter());
+		var response = Assert.IsType<OkObjectResult>(result.Result);
+		var value = Assert.IsType<Page<StaffDto>>(response.Value);
+	}
+
+	[Fact]
+	public async Task Filter_ShouldReturnNotFound_WhenNotFound()
+	{
+		_staffRepoMock.Setup(r => r.FilterStaffsAsync(It.IsAny<StaffFilter>()))
+			.ThrowsAsync(new Api.Application.Exceptions.EntityNotFoundException());
+
+		var result = await _controller.Filter(new StaffFilter());
+		Assert.IsType<NotFoundResult>(result.Result);
+	}
+
+	[Fact]
+	public async Task Filter_ShouldReturnBadRequest_WhenOtherException()
+	{
+		_staffRepoMock.Setup(r => r.FilterStaffsAsync(It.IsAny<StaffFilter>()))
+			.ThrowsAsync(new Exception("Test Exception"));
+
+		var result = await _controller.Filter(new StaffFilter());
+		Assert.IsType<BadRequestResult>(result.Result);
+	}
+	
 }
