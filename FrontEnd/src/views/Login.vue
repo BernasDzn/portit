07c8e1fbf1a93904e578f3953d513ef848486873
BernasDzn <bernardogranjacardoso@gmail.com>
2025-10-31 23:02:35 +0000

    feat(FrontEnd): add login page polka dot animation
    refs: #85 #86

--- FrontEnd/src/views/Login.vue
+++ FrontEnd/src/views/Login.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import LoginBox from "@/components/LoginBox.vue";
+import PolkaGrid from '@/components/PolkaCanvas.vue'
 import "./../assets/login.css";
 
 </script>
@@ -7,7 +8,9 @@ import "./../assets/login.css";
 <template>
     <div class="login-layout">
         <div class="login-left">
-            <img src="/PORTIT Dark Logo.svg" alt="Portit logo">
+      <!-- Canvas anchored to left panel and placed behind the logo -->
+      <PolkaGrid class="polka-bg" />
+      <img src="/PORTIT Dark Logo.svg" alt="Portit logo" class="logo" />
         </div>
         <div class="login-right">
             <LoginBox />
@@ -15,3 +18,41 @@ import "./../assets/login.css";
         </div>
     </div>
 </template>
+
+
+<style>
+.login-layout {
+  position: relative;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+.polka-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  pointer-events: none;
+}
+
+.login-left {
+  position: relative;
+  overflow: hidden;
+}
+
+.login-right {
+  position: relative;
+}
+
+.login-left > *:not(.polka-bg) {
+  position: relative;
+  z-index: 1;
+}
+
+.logo {
+  position: relative;
+  z-index: 1;
+}
+</style>
