9f59bdf5c094e6117628ee828917a7d34bb780e0
Rui Santiago <1221402@isep.ipp.pt>
2025-11-01 12:41:52 +0000

    chore(layout): add DataTable component for entity visualization
    Refs: #85, #86

--- FrontEnd/src/views/Staff.vue
+++ FrontEnd/src/views/Staff.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 	import { ref, onMounted } from 'vue'
-	import Loading from '@/components/Loading.vue'
 	import type { Staff } from '@/model/Staff'
 	import { StaffService } from '@/service/StaffService'
 	import AxiosHttpService from '@/service/AxiosHttpService'
-	import { StaffMapper } from '@/model/mappers/StaffMapper'
+	import Loading from '@/components/Loading.vue'
+	import DataTable from '@/components/DataTable.vue'
 
 	const http = new AxiosHttpService()
 	const staffService = new StaffService(http as any)
@@ -43,21 +43,14 @@
 
 		<section style="margin-top:1rem">
 		<div v-if="loading">
-			<Loading/>
+			<Loading message="Loading staff list…" />
 		</div>
 		<div v-else-if="error" class="error">Error: {{ error }}</div>
 		<div v-else>
-			<div v-if="staffs.length === 0"> No staff members found. </div>
-			<!--
-				Ofc here should go a table that we populate dynamically but getting the
-				http requests working took so long this started to PMO
-			-->
-			<ul v-else>
-			<li v-for="s in staffs" :key="s.mechanograficNumber">
-				{{ s.mechanograficNumber || 'well this is unexpected... this staff doesnt have a mechanograficNumber... how...' }} -
-				{{ s.name || 'well this is unexpected... this staff doesnt have a name.' }}
-			</li>
-			</ul>
+			<DataTable
+				:columns="['mechanograficNumber', 'name', 'email', 'phoneNumber']"
+				:rows="staffs"
+			/>
 		</div>
 		</section>
 	</div>
