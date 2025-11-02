c1cebd649fd784e745ed571bac6e6ba5c75366e3
F r a n c i s c o <1231092@isep.ipp.pt>
2025-11-02 01:29:06 +0000

    chore: add vessel type service
    Refs: #85 #86

---
+++ FrontEnd/src/service/IService/IVesselTypeService.ts
@@ -0,0 +1,8 @@
+import type { Filter, Page } from "@/model/Page";
+import { type VesselType } from "../../model/VesselType";
+
+export interface IVesselTypeService {
+    getVesselTypes(filtering: Filter<VesselType>): Promise<Page<VesselType>>
+    createVesselType(vesselType: VesselType): Promise<VesselType>;
+    updateVesselType(name: string, vesselType: VesselType): Promise<VesselType>;
+}
---
+++ FrontEnd/src/service/VesselTypeService.ts
@@ -0,0 +1,42 @@
+import { inject, injectable } from 'inversify';
+import { TYPES } from '@/inversify/types';
+
+import type { VesselType } from '@/model/VesselType';
+import type { IVesselTypeService } from './IService/IVesselTypeService';
+import type { IHttpService } from './IService/IHttpService';
+import type { Filter, Page } from '@/model/Page';
+
+@injectable()
+export class VesselTypeService implements IVesselTypeService {
+
+    constructor(
+        @inject(TYPES.api)
+        private http: IHttpService
+    ) { }
+
+    async getVesselTypes(filtering?: Filter<VesselType>): Promise<Page<VesselType>> {
+
+        let query: string[] = [];
+
+        if (filtering) {
+            query.push(filtering.filter.name ? `Name=${filtering.filter.name}&` : '');
+            query.push(filtering.filter.description ? `Description=${filtering.filter.description}&` : '');
+            query.push(filtering.pageNumber !== undefined ? `PageNumber=${filtering.pageNumber}&` : '');
+            query.push(filtering.pageSize !== undefined ? `PageSize=${filtering.pageSize}` : '');
+        }
+
+        const res = await this.http.get<Page<VesselType>>(`/VesselType/filter${query.length ? `?${query.join('')}` : ''}`);
+
+        return res.data;
+    }
+
+    async createVesselType(vesselType: VesselType): Promise<VesselType> {
+        const res = await this.http.post<VesselType>('/VesselType', vesselType);
+        return res.data;
+    }
+
+    async updateVesselType(name: string, vesselType: VesselType): Promise<VesselType> {
+        const res = await this.http.put<VesselType>(`/VesselType/${name}`, vesselType);
+        return res.data;
+    }
+}
