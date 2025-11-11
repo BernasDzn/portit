32791b91b744a00f24ebd73b9c88fe3374aa4e46
BernasDzn <bernardogranjacardoso@gmail.com>
2025-11-11 22:19:39 +0000

    fix: change prolog config to real ports

--- SchedulingNPlanning/config.pl
+++ SchedulingNPlanning/config.pl
@@ -1,4 +1,4 @@
 :- module(config, [api_url/1, frontend_url/1]).
 
-api_url("http://localhost:5195").
-frontend_url("http://localhost:5173").
+api_url("http://localhost:2226").
+frontend_url("http://localhost:2229").
