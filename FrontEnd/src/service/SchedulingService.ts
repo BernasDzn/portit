81ef37e29a43e1f84d029e106f75720790406714
BernasDzn <bernardogranjacardoso@gmail.com>
2025-11-11 22:45:08 +0000

    fix: change prolog to exposed dei port

--- FrontEnd/src/service/SchedulingService.ts
+++ FrontEnd/src/service/SchedulingService.ts
@@ -57,7 +57,7 @@ export class SchedulingService implements ISchedulingService {
 
     async scheduleForDay(day: Date, dock: string, alg: string, daysAhead: number = 2): Promise<Schedule> {
         const dayString = day.toISOString().split('T')[0];
-        const url = `http://localhost:8080/schedule?day=${dayString}&dock=${dock}&alg=${alg}&daysAhead=${daysAhead}`;
+        const url = `https://vs-gate.dei.isep.ipp.pt:30228/schedule?day=${dayString}&dock=${dock}&alg=${alg}&daysAhead=${daysAhead}`;
         const res = await this.http.getWithoutCredentials(url);
         return res.data as Schedule;
     }
--- SchedulingNPlanning/main.pl
+++ SchedulingNPlanning/main.pl
@@ -3,6 +3,6 @@
 :- consult('http-server.pl').
 
 run :-
-	URL = 'https://localhost:2228/Staff',
-	PORT = 8080,
+	URL = 'https://vs-gate.dei.isep.ipp.pt:10228/Staff',
+	PORT = 2228,
 	open_server(PORT).
