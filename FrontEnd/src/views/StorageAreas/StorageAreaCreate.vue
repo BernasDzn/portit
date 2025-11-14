47fa0c3c89e0042730070fd8095f1c5bcc2914e4
Rui Santiago <1221402@isep.ipp.pt>
2025-11-14 22:46:24 +0000

    feat: add the option to select storage area type on creation
    Refs: #130

--- FrontEnd/src/views/StorageAreas/StorageAreaCreate.vue
+++ FrontEnd/src/views/StorageAreas/StorageAreaCreate.vue
@@ -10,6 +10,7 @@ import type { IStorageAreaService } from '@/service/IService/IStorageAreaService
 import TYPES from '@/inversify/types';
 import type { IDockService } from '@/service/IService/IDockService';
 import type { StorageAreaDto } from '@/model/dto/StorageAreaDto';
+import { StorageArea } from '@/model/StorageArea';
 
 const storageAreaService = container.get<IStorageAreaService>(TYPES.storageAreaService);
 const dockService = container.get<IDockService>(TYPES.dockService);
@@ -44,6 +45,10 @@ function updateDockRelations(dockCodes: string[]) {
     });
 }
 
+function updateType(typeValue: string) {
+    storageArea.value.type = parseInt(typeValue);
+}
+
 onMounted(() => {
     dockService.getDocks().then(page => {
         allDocks.value = page.items || [];
@@ -65,24 +70,25 @@ const submitStorageArea = (obj: any) =>
         <h1 class="title">{{ t('storageArea.tabs.create') }}</h1>
         <p class="subtitle">{{ t('storageArea.subtitle.create') }}</p>
         <EntityForm :object="storageArea" :submit-function="submitStorageArea">
-            <div class="form" style="display: flex; flex-wrap: wrap;">
-                <div class="general-info">
-                    <p class="section-title">{{ t('dock.generalFields') }}</p>
-                    <FormField class="field" :name="t('storageArea.fields.nameCode.title')" v-model="storageArea.nameCode" :placeholderText="t('storageArea.fields.nameCode.placeholder')" required pattern="^[a-zA-Z0-9]*$"/>
-                    <FormField class="field" :name="t('storageArea.fields.location.title')" v-model="storageArea.location" :placeholderText="t('storageArea.fields.location.placeholder')" required/>
-                </div>
-
-                <span class="section-divider"></span>
+            <div class="form-fields">
+                <FormField class="field" :name="t('storageArea.fields.nameCode.title')" v-model="storageArea.nameCode" :placeholderText="t('storageArea.fields.nameCode.placeholder')" required pattern="^[a-zA-Z0-9]*$"/>
+                <FormField class="field" :name="t('storageArea.fields.location.title')" v-model="storageArea.location" :placeholderText="t('storageArea.fields.location.placeholder')" required/>
                 
-                <div class="measurements">
-                    <p class="section-title">{{ t('storageArea.fields.capacity.title') }}</p>
-                        <FormField class="field" :name="t('storageArea.fields.capacity.title')" v-model.number="storageArea.capacity" :placeholderText="t('storageArea.capacity.placeholder')" pattern="^[0-9]\d*$" required/>
-                        <FormField class="field" :name="t('storageArea.fields.occupancy.placeholder')" v-model.number="storageArea.currentOccupancy" :placeholderText="t('storage-areas.currentOccupancy.placeholder')" pattern="^[0-9]\d*$" required/>
+                <div class="field">
+                    <label>{{ t('storageArea.fields.type.title') }}</label>
+                    <sl-select 
+                        @sl-change="updateType($event.target.value)" 
+                        :placeholder="t('storageArea.fields.type.placeholder')"
+                        required
+                    >
+                        <sl-option v-for="(typeName, typeKey) in StorageArea.sa_type" :key="typeKey" :value="typeKey">{{ t(`storageArea.fields.type.options.${typeName.toLowerCase()}`) }}</sl-option>
+                    </sl-select>
                 </div>
 
-                <span class="section-divider"></span>
+                <FormField class="field" :name="t('storageArea.fields.capacity.title')" v-model.number="storageArea.capacity" :placeholderText="t('storageArea.capacity.placeholder')" pattern="^[0-9]\d*$" required/>
+                <FormField class="field" :name="t('storageArea.fields.occupancy.placeholder')" v-model.number="storageArea.currentOccupancy" :placeholderText="t('storage-areas.currentOccupancy.placeholder')" pattern="^[0-9]\d*$" required/>
 
-                <div class="measurements" style="flex: 100%;">
+                <div style="flex:100%; width: 100%;">
                     <p class="section-title">{{ t('dock.title') }}</p>
                     <EntityDropdown
                         class="field-dropdown"
@@ -90,11 +96,11 @@ const submitStorageArea = (obj: any) =>
                         :fetch-function="() => dockService.getDocks().then(page => (page.items || []).map(t => t.code))"
                         :fetch-on-mount="true"
                         :placeholderText="t('physicalResource.fields.servingDocks.placeholder')"
-                        :required="true"
+                        :required="false"
                         :multiple="true"
                         valueKey="code"
                         labelKey="name"
-                        @sl-change="console.log($event.target.value), updateDockRelations($event.target.value)"
+                        @sl-change="updateDockRelations($event.target.value)"
                     />
                     <div style="display: flex; flex-wrap: wrap; gap: 1rem;">
                         <sl-card class="card-header" style="width: fit-content;" v-for="dock in storageArea.dockServices" :key="dock.dockCode" >
@@ -111,31 +117,27 @@ const submitStorageArea = (obj: any) =>
 </template>
 
 <style scoped>
-
-.form{
+.form-fields {
     display: flex;
-    flex-direction: row;
-}
-
-.general-info {
-    display: flex;
-    flex-direction: column;
-}
-
-.measurements {
-    display: flex;
-    flex-direction: column;
-}
-
-.measurements-grid {
-    display: flex;
-    flex-direction: row;
+    gap: .5rem;
+    flex-wrap: wrap;
 }
 
 .field {
     margin-bottom: 1rem;
     max-width: 30rem;
     padding: .5rem
+}
+
+.field label {
+    display: block;
+    margin-bottom: 0.5rem;
+    font-weight: 500;
+    color: var(--sl-color-neutral-700);
+}
+
+.field sl-select {
+    width: 100%;
 }
 
 .field-dropdown {
@@ -146,12 +148,6 @@ const submitStorageArea = (obj: any) =>
     margin-top: 0.5rem;
     margin-left: 0.5rem;
     max-width: 30rem;
-}
-
-.section-divider {
-    width: 1px;
-    margin: 0 1rem;
-    background-color: var(--sl-color-neutral-200);
 }
 
 .section-title {
