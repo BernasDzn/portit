c17fcae9b54f71b387011165496932ef4dba81c3
BernasDzn <bernardogranjacardoso@gmail.com>
2025-11-14 21:50:49 +0000

    fix: change prolog to external api and frontend url

--- SchedulingNPlanning/config.pl
+++ SchedulingNPlanning/config.pl
@@ -1,7 +1,7 @@
 :- module(config, [api_url/1, frontend_url/1]).
 
-api_url("http://localhost:2226").
-frontend_url("http://localhost:2229").
+api_url("https://vs-gate.dei.isep.ipp.pt:10228").
+frontend_url("https://vs-gate.dei.isep.ipp.pt:40228").
 
 % api_url("http://localhost:5195").
 % frontend_url("http://localhost:5173").
