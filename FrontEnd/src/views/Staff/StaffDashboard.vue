a229a889cccf3ed339e9560e23c620218c1da038
F r a n c i s c o <1231092@isep.ipp.pt>
2025-11-21 21:53:28 +0000

    feat: add staff statistics to staff dashboard

--- FrontEnd/src/views/Staff/StaffDashboard.vue
+++ FrontEnd/src/views/Staff/StaffDashboard.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
 import DashboardItem from '@/components/DashboardItem.vue';
+import Loading from '@/components/Loading.vue';
 import { useI18n } from 'vue-i18n';
+import { ref, onMounted } from 'vue';
+import type { IStaffService } from '@/service/IService/IStaffService';
+import { container } from '@/inversify.config';
+import TYPES from '@/inversify/types';
+
+const staffService = container.get<IStaffService>(TYPES.staffService);
 
 const { t } = useI18n();
+
+const numberOfStaff = ref(0);
+const loading = ref(true);
+onMounted(async () => {
+    try {
+        numberOfStaff.value = await staffService.getNumberOfStaffs();
+        loading.value = false;
+    } catch (err) {
+        console.error('Failed to load staff members', err);
+    }
+});
 </script>
 
 <template>
@@ -25,5 +43,32 @@ const { t } = useI18n();
                 />
             </div>
         </sl-card>
+        <sl-card class="dashboard-statistics">
+            <div class="stats-overview" v-if="!loading">
+                <p>{{numberOfStaff}}</p>
+                <p>{{ t('staff.registeredStaffMembers') }}</p>
+            </div>
+            <Loading v-if="loading"/>
+        </sl-card>
     </div>
 </template>
+
+<style scoped>
+.stats-overview {
+    text-align: center;
+    margin-bottom: 1rem;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    background-color: #f5f6fa;
+}
+
+.stats-overview p {
+    margin: 0;
+    padding: 0.5rem;
+    color: #485ea9;
+}
+
+.stats-overview p:nth-child(1) {
+    font-size: 2rem;
+}
+</style>
