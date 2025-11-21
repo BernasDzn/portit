89f84a0d5878e586b3196f3cf1713391b9fff4f8
F r a n c i s c o <1231092@isep.ipp.pt>
2025-11-21 21:46:00 +0000

    feat: add number of physical resources in physical resources dashboard

--- FrontEnd/src/views/PhysicalResources/PhysicalResourceDashboard.vue
+++ FrontEnd/src/views/PhysicalResources/PhysicalResourceDashboard.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
 import DashboardItem from '@/components/DashboardItem.vue';
+import Loading from '@/components/Loading.vue';
+import { container } from '@/inversify.config';
+import TYPES from '@/inversify/types';
+import type { IPhysicalResourceService } from '@/service/IService/IPhysicalResourceService';
+import { onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
+const physicalResourceService = container.get<IPhysicalResourceService>(TYPES.physicalResourceService);
+
 const { t } = useI18n();
+
+const numberOfPhysicalResources = ref(0);
+const loading = ref(true)
+onMounted(async () => {
+    try {
+        numberOfPhysicalResources.value = await physicalResourceService.getNumberOfPhysicalResources();
+        loading.value = false;
+    } catch (err) {
+        console.error('Failed to load physical resources', err);
+    }
+});
 </script>
 
 <template>
@@ -25,5 +43,32 @@ const { t } = useI18n();
                 />
             </div>
         </sl-card>
+        <sl-card class="dashboard-statistics">
+            <div class="stats-overview" v-if="!loading">
+                <p>{{ numberOfPhysicalResources }}</p>
+                <p>{{ t('physicalResource.registeredPhysicalResources') }}</p>
+            </div>
+            <Loading v-if="loading" />
+        </sl-card>
     </div>
 </template>
+
+<style scoped>
+.stats-overview {
+    text-align: center;
+    margin-bottom: 1rem;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    background-color: #f5f6fa;
+}
+
+.stats-overview p {
+    margin: 0;
+    padding: 0.5rem;
+    color: #485ea9;
+}
+
+.stats-overview p:nth-child(1) {
+    font-size: 2rem;
+}
+</style>
