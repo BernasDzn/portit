5dd18082cc5b78729e4be95995e4478047b1b199
F r a n c i s c o <1231092@isep.ipp.pt>
2025-11-21 22:00:03 +0000

    feat: add storage area statistics to storage area dashboard

--- FrontEnd/src/views/StorageAreas/StorageAreaDashboard.vue
+++ FrontEnd/src/views/StorageAreas/StorageAreaDashboard.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
 import DashboardItem from '@/components/DashboardItem.vue';
+import Loading from '@/components/Loading.vue';
+import { container } from '@/inversify.config';
+import TYPES from '@/inversify/types';
 
 import AxiosHttpService from '@/service/AxiosHttpService';
-import { ref } from 'vue';
+import type { IStorageAreaService } from '@/service/IService/IStorageAreaService';
+import { onMounted, ref } from 'vue';
 // @ts-ignore: missing type declarations for 'vue-i18n' in this project
 import { useI18n } from 'vue-i18n'
 
-const { t } = useI18n()
+const storageAreaService = container.get<IStorageAreaService>(TYPES.storageAreaService);
 
+const { t } = useI18n();
+
+const numberOfStorageAreas = ref(0);
+const loading = ref(true);
+onMounted(async () => {
+    try {
+        numberOfStorageAreas.value = await storageAreaService.getNumberOfStorageAreas();
+        loading.value = false;
+    } catch (err) {
+        console.error('Failed to load staff members', err);
+    }
+});
 </script>
 
 <template>
@@ -30,6 +46,13 @@ const { t } = useI18n()
                 />
             </div>
         </sl-card>
+        <sl-card class="dashboard-statistics">
+            <div class="stats-overview" v-if="!loading">
+                <p>{{numberOfStorageAreas}}</p>
+                <p>{{ t('storageArea.registeredStorageAreas') }}</p>
+            </div>
+            <Loading v-if="loading"/>
+        </sl-card>
     </div>
 </template>
 
@@ -44,5 +67,22 @@ const { t } = useI18n()
     display: flex;
     flex-wrap: wrap;
     gap: 1rem;
+}
+.stats-overview {
+    text-align: center;
+    margin-bottom: 1rem;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    background-color: #f5f6fa;
+}
+
+.stats-overview p {
+    margin: 0;
+    padding: 0.5rem;
+    color: #485ea9;
+}
+
+.stats-overview p:nth-child(1) {
+    font-size: 2rem;
 }
 </style>
