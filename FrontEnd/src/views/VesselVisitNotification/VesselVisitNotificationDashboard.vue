343ca9d5f5c2cdce9b539dcd325d6ac7720d196d
F r a n c i s c o <1231092@isep.ipp.pt>
2025-11-21 21:36:42 +0000

    feat: add statistics to vessel visit notifications dashboard

--- FrontEnd/src/views/VesselVisitNotification/VesselVisitNotificationDashboard.vue
+++ FrontEnd/src/views/VesselVisitNotification/VesselVisitNotificationDashboard.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
 import DashboardItem from '@/components/DashboardItem.vue';
 import { useSession } from '@/composables/session';
-import { ref } from 'vue';
+import { container } from '@/inversify.config';
+import TYPES from '@/inversify/types';
+import type { VesselVisitDistributionDto } from '@/model/dto/VesselVisitNotificationDto';
+import type { IVesselVisitNotificationService } from '@/service/IService/IVesselVisitNotificationService';
+import { onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n'
 
-const { t } = useI18n();
 const role = ref(useSession().authenticatedUser!.role);
+const vvnService = container.get<IVesselVisitNotificationService>(TYPES.vesselVisitNotificationService);
+const { t } = useI18n()
 
+const distribution = ref<VesselVisitDistributionDto>({
+    pending: 0,
+    accepted: 0,
+    rejected: 0
+});
+const numberOfVesselVisitNotifications = ref(0);
+const loading = ref(true)
+onMounted(async () => {
+    try {
+        distribution.value = await vvnService.count();
+        loading.value = false;
+        numberOfVesselVisitNotifications.value = distribution.value.accepted + distribution.value.pending + distribution.value.rejected;
+    } catch (err) {
+        console.error('Failed to load vessel visit notifications', err);
+    }
+});
 </script>
 
 <template>
@@ -43,5 +64,33 @@ const role = ref(useSession().authenticatedUser!.role);
                 />
             </div>
         </sl-card>
+        <sl-card class="dashboard-statistics">
+            <div class="stats-overview" v-if="!loading">
+                <p>{{numberOfVesselVisitNotifications}}</p>
+                <p>{{ t('notification.registeredVesselVisitNotifications') }}</p>
+            </div>
+            <Loading v-if="loading"/>
+        </sl-card>
     </div>
 </template>
+
+<style scoped>
+
+.stats-overview {
+    text-align: center;
+    margin-bottom: 1rem;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    background-color: #f5f6fa;
+}
+
+.stats-overview p {
+    margin: 0;
+    padding: 0.5rem;
+    color: #485ea9;
+}
+
+.stats-overview p:nth-child(1) {
+    font-size: 2rem;
+}
+</style>
