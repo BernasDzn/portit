1acff96c30841f2559c96448b5c8026e57c95a84
BernasDzn <bernardogranjacardoso@gmail.com>
2025-11-30 00:25:24 +0000

    fix: removed letterbox from 3d visualizer

--- FrontEnd/src/views/Visualizer.vue
+++ FrontEnd/src/views/Visualizer.vue
@@ -6,7 +6,7 @@ const height = ref(window.innerHeight - 120);
 </script>
 
 <template>
-  <div class="viewer-container" :style="`height: ${height}px;`">
+  <div class="viewer-container">
     <iframe
         src="/visualizer/index.html"
         class="viewer-iframe"
@@ -18,19 +18,19 @@ const height = ref(window.innerHeight - 120);
 <style scoped>
 .viewer-container {
 
-    position: fixed;
-    top:100px;
-    left: 0;
+  position: fixed;
+  top: 80px;
+  bottom: 0;
+  right: 0;
+  left: 0;
 
-    background: black; /* letterbox background color */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-    margin: 0;
-    padding: 0;
-
-    width: 100%;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
 }
 
 html {
@@ -38,15 +38,11 @@ html {
 }
 
 .viewer-iframe {
-
-    aspect-ratio: 16 / 9;
-    width: 100%;
-    height: auto;
-    max-width: 100%;
-    max-height: 100%;
-    border: none;
-    display: block;
-    background: black;
-    object-fit: contain;
+  width: 100%;
+  height: 100%;
+  border: none;
+  display: block;
+  background: black;
+  object-fit: contain;
 }
 </style>
