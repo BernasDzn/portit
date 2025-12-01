0f177b4fd6e04c79cb529bd9f024488528ad8159
F r a n c i s c o <1231092@isep.ipp.pt>
2025-12-01 04:41:47 +0000

    fix: conditionally display notifications based on user role

--- FrontEnd/src/components/Topbar.vue
+++ FrontEnd/src/components/Topbar.vue
@@ -7,6 +7,10 @@ import LanguageSwitcher from './LanguageSwitcher.vue';
 // @ts-ignore
 import confetti from 'canvas-confetti';
 
+import { useSession } from '@/composables/session';
+
+const user = useSession().authenticatedUser;
+
 function onLogoClick(_: MouseEvent) {
   try {
     const originX = 0.5;
@@ -49,7 +53,7 @@ function onLogoClick(_: MouseEvent) {
       <LanguageSwitcher />
 
       <!--Notifications-->
-      <Notifications />
+      <Notifications v-if="user?.role === 0"/>
 
       <!-- Profile -->
       <UserProfile />
