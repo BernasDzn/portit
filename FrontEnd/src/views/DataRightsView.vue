224c481c55465682794b2a4cd0801e16018fafaf
BernasDzn <bernardogranjacardoso@gmail.com>
2025-12-14 00:27:28 +0000

    feat: add personal data downloading and deletion
    refs: close #213

---
+++ FrontEnd/src/views/DataRightsView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import DataRights from '@/components/DataRights.vue';
+</script>
+
+<template>
+    <div>
+        <h1 class="title">My Data</h1>
+        <p class="subtitle">Manage your personal data and exercise your privacy rights</p>
+    </div>
+
+    <DataRights />
+</template>
