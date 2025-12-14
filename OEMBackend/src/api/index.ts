f617cdb07951ed71ccd2389f8cc61d3b4c14af8a
Rui Santiago <1221402@isep.ipp.pt>
2025-12-14 00:43:43 +0000

    feat: add incident type routes for incident type management
    Refs: #183, #184

--- OEMBackend/src/api/index.ts
+++ OEMBackend/src/api/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import operationPlan from './routes/operationPlanRoutes';
 import scheduleRequest from './routes/scheduleRequestRoutes';
+import incidentType from './routes/incidentTypeRoutes';
 
 export default () => {
 	const app = Router();
 
 	operationPlan(app);
 	scheduleRequest(app);
+	incidentType(app);
 	
 	return app
 }
